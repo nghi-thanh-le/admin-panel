e0b26905a27506cefcf511fbcdd5775e23789cb7
Nghi <thanhnghi94@gmail.com>
2016-06-09 12:37:33 +0200

    ...

--- app-api/routes/api.js
+++ app-api/routes/api.js
@@ -3,8 +3,6 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var adminUser = require('../models/adminUser');
 var path = require('path');
-var bcrypt = require('bcrypt-nodejs');
-var TokenAuth = require('./TokenAuth');
 var app = express();
 var fs = require('fs');
 var multer = require('multer');
@@ -161,7 +159,7 @@ router.post('/uploadPicture', upload.single('file'), function(req, res) {
             message: 'Unable to saved empty image'
         });
     }
-    
+
     Products.findById({
         _id: req.body._id
     }, function(err, product) {
@@ -203,54 +201,5 @@ router.get('/delete/:_id', function(req, res) {
         });
     });
 });
-
-// router.post('/createUser', function (req, res) {
-//     adminUser.findOne({
-//         username: req.body.username
-//     }, function (err, user) {
-//         if(err) {
-//             return sendJsonResponse(res, 401, {message: "Error occur " + err});
-//         }
-//         if(user) {
-//             return sendJsonResponse(res, 401, {message: "User already exists!"});
-//         } else {
-//             var AdminUser = new adminUser();
-//             var salt, hash, password;
-//             password = req.body.password;
-//             salt = bcrypt.genSaltSync(10);
-//             hash = bcrypt.hashSync(password, salt);
-//
-//             AdminUser.username = req.body.username;
-//             AdminUser.password = hash;
-//             AdminUser.save(function (err, user) {
-//                 sendJsonResponse(res, 200, TokenAuth.generateToken(user._id));
-//             });
-//         }
-//     })
-// });
-//
-// router.post('/login', function (req, res) {
-//     var username = req.body.username;
-//     var password = req.body.password;
-//
-//     adminUser.findOne({
-//         username: username
-//     }, function (err, data) {
-//         if (err | data === null) {
-//             return sendJsonResponse(res, 404, { message: "User not found"});
-//         } else {
-//             var user = data;
-//             if(username === user.username && bcrypt.compareSync(password, user.password)) {
-//                 return sendJsonResponse(res, 200, TokenAuth.generateToken(user._id));
-//             } else {
-//                 return sendJsonResponse(res, 401, {message: 'Bad username or password'});
-//             }
-//         }
-//     });
-// });
-//
-// router.get('/logout', function(req, res) {
-//     sendJsonResponse(res, 401, {message: 'User logged out'});
-// });
 
 module.exports = router;
